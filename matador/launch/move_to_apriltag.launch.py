24c7dd30ad402eedd41fca0e1a4136d7c29562e9
fire-lap <lord.daniel.w@gmail.com>
2025-10-26 17:15:06 -0400

    launch all from launch file

--- matador/launch/move_to_apriltag.launch.py
+++ matador/launch/move_to_apriltag.launch.py
@@ -1,10 +1,49 @@
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
+    # Remote TurtleBot3 bringup on 192.168.50.39
+    remote_turtlebot_bringup = ExecuteProcess(
+        cmd=['ssh', 'fire@192.168.50.39', 
+             'source /opt/ros/humble/setup.bash && ros2 launch turtlebot3_bringup robot.launch.py'],
+        shell=False,
+        output='screen'
+    )
+    
+    # TurtleBot3 Gazebo empty world
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('turtlebot3_gazebo'),
+                'launch',
+                'empty_world.launch.py'
+            )
+        ])
+    )
+    
+    # TurtleBot3 Cartographer
+    cartographer_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('turtlebot3_cartographer'),
+                'launch',
+                'cartographer.launch.py'
+            )
+        ]),
+        launch_arguments={'use_sim_time': 'True'}.items()
+    )
+    
+    # Map saver
+    map_saver = ExecuteProcess(
+        cmd=['ros2', 'run', 'nav2_map_server', 'map_saver_cli', '-f', os.path.expanduser('~/map')],
+        output='screen'
+    )
+    
     # Get the path to the config file
     config_dir = os.path.join(
         get_package_share_directory('matador'),
@@ -52,6 +91,10 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        remote_turtlebot_bringup,
+        # gazebo_launch,
+        # cartographer_launch,
+        # map_saver,
         # camera_info_publisher_node,
         apriltag_node,
         move_to_apriltag_node,
